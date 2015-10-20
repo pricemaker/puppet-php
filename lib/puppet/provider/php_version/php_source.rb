008b60889f5414773c24cc3d61b82231c928ebc8
Patrick Hindmarsh <patrick@hindmar.sh>
2015-10-20 15:58:59 +1300

    Don't use hardcoded homebrew path for bison26
    Default homebrew path has been changed to `/usr/local` which means this no longer works.

--- lib/puppet/provider/php_version/php_source.rb
+++ lib/puppet/provider/php_version/php_source.rb
@@ -170,7 +170,7 @@ Puppet::Type.type(:php_version).provide(:php_source) do
 
     # PHP 5.5+ requires a later version of Bison than OSX provides (2.6 vs 2.3)
     if Gem::Version.new(@resource[:version]) > Gem::Version.new('5.4.17')
-      env << " && export PATH=/opt/boxen/homebrew/opt/bisonphp26/bin:$PATH"
+      env << " && export PATH=#{@resource[:homebrew_path]}/opt/bisonphp26/bin:$PATH"
     end
 
     # Construct and run configure command
@@ -192,7 +192,7 @@ Puppet::Type.type(:php_version).provide(:php_source) do
   def make
     # PHP > 5.4.17 requires a later version of Bison than OSX provides (2.6 vs 2.3)
     if Gem::Version.new(@resource[:version]) > Gem::Version.new('5.4.17')
-      env = " && export PATH=/opt/boxen/homebrew/opt/bisonphp26/bin:$PATH"
+      env = " && export PATH=#{@resource[:homebrew_path]}/opt/bisonphp26/bin:$PATH"
     end
 
     puts %x( cd #{@resource[:phpenv_root]}/php-src/ #{env} && make 2>&1 )
